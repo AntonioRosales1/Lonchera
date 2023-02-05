93e50924ee43502dca2d7b9474b3654a33fa59bb
AntonioRosales1 <elrluis213@gmail.com>
2023-02-04 16:32:32 -0800

    responsice css

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// var i = 0;
+// var dealOfDay = [];
+// var time = 5000;
 
+// dealOfDay[0] = `./imgs/breakfast.jpeg`;
+// dealOfDay[1] = `./imgs/breakfast.jpeg`;
+// dealOfDay[2] = `./imgs/breakfast.jpeg`;
+
+// function changeImg() {
+//   document.hasChildNodes.src = dealOfDay[i];
+//   if (i < dealOfDay.length - 1) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+// }
